78f06e3d04df7180166b1eb0950193df78db2c5e
Kylo Ginsberg <kylo@puppet.com>
2016-09-02 11:38:03 -0700

    Create a 'file_format' field by default
    Since people may come to one of these MAINTAINERS files without
    any context, add a 'file_format' field at the top-level referring
    back to this repository. The field is optional but created by
    default with 'maintainers create'.

--- lib/maintainers/runner.rb
+++ lib/maintainers/runner.rb
@@ -60,6 +60,7 @@ module Maintainers
       # minimum content for a maintainers file
       maintainers = {}
       maintainers["version"] = 1
+      maintainers["file_format"] = "This MAINTAINERS file format is described at https://github.com/puppetlabs/maintainers"
       maintainers["issues"] = options[:issues]
       maintainers["people"] = []
 
